442b3eced55624c3f4879dfa20e5a862f5262385
Samuel Popjak <samuelpopjak@outlook.sk>
2026-02-26 09:07:58 +0200

    nextjsapp commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,90 @@
-import Image from "next/image";
+'use client'
+
+import { useState, useEffect } from "react";
+
+interface StampData {
+  merchantName: string,
+  customerName: string,
+  currentStamps: number,
+  stampsRequired: number,
+  rewardAvailable: boolean,
+}
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+  const [stampsData, setStampsData] = useState<StampData | undefined>()
+  
+  const fetchStampData = async () => {
+    try {
+      const response = await fetch('http://localhost:5001/merchants/m1/customers/c1/card')
+      const stamps = await response.json()
+      setStampsData(stamps)
+    } catch (error) {
+      console.log("Fetch error: ", error)
+    }
+  }
+
+  useEffect(() => {
+    const fetchStampData = async () => {
+    try {
+      const response = await fetch('http://localhost:5001/merchants/m1/customers/c1/card')
+      const stamps = await response.json()
+      setStampsData(stamps)
+    } catch (error) {
+      console.log("Fetch error: ", error)
+    }
+    }
+    fetchStampData()
+  }, []);
+
+  function getStamps() {
+    const content = []
+    if (!stampsData) {
+      return <div>Could not fetch</div>
+    }
+    if (stampsData.currentStamps === stampsData.stampsRequired) {
+      content.push(<li key={"reward"}>🎉 Reward Available!</li>)
+      return content;
+    }
+
+    for (let i = 0; i < stampsData.currentStamps; i++) {
+      content.push(<li key={i}>✅</li>)
+    }
+    for (let j = stampsData.currentStamps; j < stampsData.stampsRequired; j++) {
+      content.push(<li key={j}>[]</li>)
+    }
+    return content;
+  }
+
+  const addStamp = async () => {
+    const response = await fetch('http://localhost:5001/merchants/m1/customers/c1/stamps', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({"count": 1})
+    })
+    if (response.ok) {
+      fetchStampData();
+    }
+  }
+
+  if (stampsData === undefined) {
+      return <div>Could not fetch <data value=""></data></div>
+  } else {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen bg-black font-sans text-center">
+        <div>
+          <p className="text-white">{stampsData.merchantName}</p>
+          <p className="text-white">Customer: {stampsData.customerName}</p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <p className="text-white">{stampsData.currentStamps}/{stampsData.stampsRequired}</p>
+        <div className="flex justify-center w-full">
+          <ul className="flex flex-row flex-wrap justify-center gap-4">
+            {getStamps()}
+          </ul>
         </div>
-      </main>
-    </div>
-  );
+        <button className="text-white" onClick={() => addStamp()}>Add stamp</button>
+      </div>
+    );
+  }
 }
